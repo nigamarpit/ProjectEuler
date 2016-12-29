8552499d6098495d96b34d42fa895c52a35367cf
Arpit Nigam <arpit.nigam@outlook.com>
2016-12-29 08:51:20 -0800

    type:feature Solution 10

--- Solutions/Euler10.py
+++ Solutions/Euler10.py
@@ -1,4 +1,16 @@
 #Summation of primes
-def Euler10():
+def Euler10(num):
+	l=[1]*(num+1)
+	s=0
+	for i in range(2,(num+1)):
+		if l[i]==0:
+			continue
+		print(i)
+		s+=i
+		y=1
+		while i*y<(num+1):
+			l[i*y]=0
+			y+=1
+	return s
 
-print(Euler10())
+print(Euler10(2000000))
