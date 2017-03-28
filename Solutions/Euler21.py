49804de8971c473957617c9d3152bc302d0ded42
Arpit Nigam <arpit.nigam@outlook.com>
2017-03-28 09:23:09 -0700

    type:feature Solution 21

---
+++ Solutions/Euler21.py
@@ -0,0 +1,39 @@
+d=dict()
+def AmicableNumbers():
+	sumAmicable=0
+	for i in range(1,10001):
+		if checkAmicable(i):
+			sumAmicable+=i
+			print(i,True)
+			continue
+		#print(i,False)
+	print(sumAmicable)
+
+def checkAmicable(n):
+	#input(n)
+	b=0
+	if d.get(n,False):
+		b=d[n]
+	else:
+		b=sumProperDivisor(n)
+		d[n]=b
+	a=sumProperDivisor(b)
+	if a==n:
+		if a==b:
+			return False
+		else:
+			return True
+	else:
+		return False
+
+
+
+def sumProperDivisor(n):
+	sumDivisors=0
+	for i in range(1,int((n+2)/2)):
+		if n%i==0:
+			sumDivisors+=i
+	return sumDivisors
+
+	
+AmicableNumbers()
